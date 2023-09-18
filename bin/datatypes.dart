b60c65df1b9d7354ba2a4f152bf4fd231c240a17
vishnuvish333 <vishnubruze5@gmail.com>
2023-09-18 13:14:14 +0530

    commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name   = 'vishnu';
+  int age       = 20;
+  int phone     = 9874563210;
+  double cgpa   = 8.8;
+  String email  = 'vishnu123@gmail.com';
+
+  print('my name is $name');
+  print('i am $age of old');
+  print('my phone number is $phone');
+  print('$cgpa is my cgpa in graduation');
+  print('my email is $email');
+}
